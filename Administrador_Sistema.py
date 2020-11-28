4528ad934243db7d7156f9941ca2dc21ff360056
OwenJaureguiB <71340202+OwenJaureguiB@users.noreply.github.com>
2020-11-27 22:31:48 -0600

    Diseño basico de GUI

--- Administrador_Sistema.py
+++ Administrador_Sistema.py
@@ -1,2 +1,17 @@
-import tkinter as tk
+import Tkinter as tk
 
+class App(Frame):
+    def __init__(self, master = None):
+        Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.quitButton = Button (self, text = "Close", command = self.quit)
+        self.quitButton.grid()
+
+if __name__ == "__main__":
+    app = App()
+    app.master.title("Administrador del Sistema")
+    app.mainloop()
+    pass
