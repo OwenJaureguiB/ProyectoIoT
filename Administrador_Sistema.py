df351fc17047e0641a061668e1b405ec26433be5
OwenJaureguiB <71340202+OwenJaureguiB@users.noreply.github.com>
2020-11-30 19:48:14 -0600

    Se hace el inicio de sesión con el usuario dueño en la base de datos

--- Administrador_Sistema.py
+++ Administrador_Sistema.py
@@ -1,14 +1,19 @@
 from tkinter import *
+import firebase_admin
+from firebase_admin import credentials, db, storage, firestore
 
 class App(Frame):
     def __init__(self, master = None):
         Frame.__init__(self, master)
         top=self.winfo_toplevel()
-        self.usuario = "yo"
-        self.contra  = "Prueba"
         self.grid()
+        self.firebaseConnection()
         self.createWidgets()
         self.login()
+
+    def firebaseConnection(self):
+        cred = credentials.Certificate("controlacceso-iot-firebase-adminsdk-s0oyr-e0fd69aa2b.json")
+        firebase_admin.initialize_app(cred, {"databaseURL":"https://controlacceso-iot.firebaseio.com/"})
 
     def createWidgets(self):
         self.user  = Label(self,text="User:")
@@ -19,6 +24,7 @@ class App(Frame):
         self.quitButton  = Button(self,text="Close",command=self.quit)
     
     def login(self):
+        self.admin = db.reference("Admin").get()
         self.user.grid()
         self.user_.grid(row=0,column=1)
         self.pasword.grid()
@@ -31,12 +37,22 @@ class App(Frame):
         us = self.user_.get()
         if(len(pw) == 0 or len(us) == 0):
             print("Datos incompletos")
-        elif(self.contra == pw and self.usuario == us):
+        elif(self.admin["Pasword"] == pw and self.admin["Usuario"] == us):
             print("Correcto")
+            self.administrar()
         else:
             print("Incorrecto")
             self.user_.delete(0,last=len(us))
             self.pasword_.delete(0,last=len(pw))
+
+    def administrar(self):
+        self.users = db.reference("Usuarios")
+        self.user.grid_forget()
+        self.user_.grid_forget()
+        self.pasword.grid_forget()
+        self.pasword_.grid_forget()
+        self.loginButton.grid_forget()
+
 
 app = App()
 app.master.title("Administrador del Sistema")
