5652ed1c261ba94bd056f703cc1c16c3d21d3472
OwenJaureguiB <71340202+OwenJaureguiB@users.noreply.github.com>
2020-12-01 21:58:34 -0600

    creación de usuarios

--- Administrador_Sistema.py
+++ Administrador_Sistema.py
@@ -1,10 +1,10 @@
 import os
 import cv2
-import firebase_admin
 import numpy as np
-from PIL import Image
+from PIL import Image as image
+import firebase_admin
 from tkinter import *
-from firebase_admin import credentials, db, storage, firestore
+from firebase_admin import credentials, db, storage
 
 class App(Frame):
     def __init__(self, master = None):
@@ -16,13 +16,18 @@ class App(Frame):
 
     def firebaseConnection(self):
         cred = credentials.Certificate("controlacceso-iot-firebase-adminsdk-s0oyr-e0fd69aa2b.json")
-        firebase_admin.initialize_app(cred, {"databaseURL":"https://controlacceso-iot.firebaseio.com/"})
+        firebase_admin.initialize_app(cred, {"databaseURL":"https://controlacceso-iot.firebaseio.com/","storageBucket": "controlacceso-iot.appspot.com"})
+        self.bucket = storage.bucket()
 
     def createWidgets(self):
         self.user  = Label(self,text="Usuario:")
         self.user_ = Entry(self)
         self.pasword  = Label(self,text="Contraseña:")
         self.pasword_ = Entry(self,show='*',exportselection=0)
+        self.nombre  = Label(self,text="Nombre:")
+        self.nombre_ = Entry(self)
+        self.apellido  = Label(self,text="Apellido:")
+        self.apellido_ = Entry(self)
         self.usuarios = Label(self,text="Usuarios")
         self.acceso = Label(self,text="Acceso Permitido")
         self.loginButton = Button(self,text="Login",command=self.checkLog)
@@ -33,13 +38,14 @@ class App(Frame):
         self.agregarPerm = Button(self,text="Ver Habitacion",command=self.agregarPermiso)
         self.giveAcc = Button(self,text="Otorgar Permiso",command=self.giveAccess)
         self.removeAcc = Button(self,text="Remover Permiso",command=self.removeAccess)
+        self.SetUser = Button(self,text="Crear Usuario",command=self.getUserData)
         self.yScroll = Scrollbar(self, orient=VERTICAL)
         self.listaRooms = Listbox(self,selectmode=SINGLE,yscrollcommand=self.yScroll.set,width=40)
         self.yScroll2 = Scrollbar(self, orient=VERTICAL)
         self.usuarios_sin_permiso = Listbox(self,selectmode=MULTIPLE,yscrollcommand=self.yScroll2.set,width=40)
         self.yScroll3 = Scrollbar(self, orient=VERTICAL)
         self.usuarios_con_permiso = Listbox(self,selectmode=MULTIPLE,yscrollcommand=self.yScroll3.set,width=40)
-    
+
     def login(self):
         self.admin = db.reference("Admin").get()
         self.user.grid()
@@ -63,10 +69,10 @@ class App(Frame):
             self.pasword_.delete(0,last=len(pw))
 
     def administrar(self):
-        self.dbUsers = db.reference("Usuarios")
         self.dbRooms = db.reference("Habitacion")
-        self.users = self.dbUsers.get()
         self.rooms = self.dbRooms.get()
+        self.dbUsers = db.reference("Usuarios")
+        self.users = self.dbUsers.get()
         self.user.grid_forget()
         self.user_.grid_forget()
         self.pasword.grid_forget()
@@ -78,16 +84,40 @@ class App(Frame):
         self.adminUsers.grid(row=3,columnspan=2,sticky=N+S+E+W)
         self.quitButton.grid(column=1, sticky=E)
 
+    def forgetLog(self):
+        self.newUser.grid_forget()
+        self.removeUser.grid_forget()
+        self.adminUsers.grid_forget()
+
     def createUser(self):
-        execfile("01_face_dataset.py")
+        self.forgetLog()
+        self.nombre.grid(row=0)
+        self.nombre_.grid(row=0,column=1)
+        self.apellido.grid(row=1)
+        self.apellido_.grid(row=1,column=1)
+        self.SetUser.grid(row=2,columnspan=2,sticky=N+S+E+W)
+        self.quitButton.grid_forget()
+        self.quitButton.grid(row=3,column=1,sticky=E)
+        
+    def getUserData(self):
+        self.lectura()
+        self.train()
+        blob = self.bucket.blob("trainer.yml")
+        blob.upload_from_filename("trainer/trainer.yml")
+        newUser = "Usuario" + str(len(self.users))
+        permisos = {}
+        for i in range(len(self.rooms)):
+            permisos["Permiso"+str(i)]=False
+        self.dbUsers.child(newUser).set({"Nombre":self.nombre_.get(), 
+                     "Apellido":self.apellido_.get(),
+                     "Permisos":permisos}
+        )
 
     def delUser(self):
         print(self.users)
 
     def access(self):
-        self.newUser.grid_forget()
-        self.removeUser.grid_forget()
-        self.adminUsers.grid_forget()
+        self.forgetLog()
         self.yScroll.grid( row=0, column=1, sticky=N+S )
         self.listaRooms.grid( row=0, column=0, sticky=N+S+E+W )
         self.yScroll["command"] = self.listaRooms.yview
@@ -152,7 +182,7 @@ class App(Frame):
         face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
         # For each person, enter one numeric face id
-        face_id = input('\n enter user id end press <return> ==>  ')
+        face_id = len(self.users)
 
         print("\n [INFO] Initializing face capture. Look the camera and wait ...")
         # Initialize individual sampling face count
@@ -171,7 +201,8 @@ class App(Frame):
 
                 # Save the captured image into the datasets folder
                 cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
-
+                #blob = self.bucket.blob(str(face_id)+"/User." + str(face_id) + '.' + str(count) + ".jpg")
+                #blob.upload_from_filename("dataset/User." + str(face_id) + '.' + str(count) + ".jpg")
                 cv2.imshow('image', img)
 
             k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
@@ -185,29 +216,19 @@ class App(Frame):
         cam.release()
         cv2.destroyAllWindows()
 
-    def training(self):
-        print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
-        faces,ids = getImagesAndLabels(path)
-        recognizer.train(faces, np.array(ids))
+    def getImagesAndLabels(self,path):
 
-        # Save the model into trainer/trainer.yml
-        recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
-
-        # Print the numer of faces trained and end program
-        print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
-
-    def getImagesAndLabels(self, path):
         imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
         faceSamples=[]
         ids = []
 
         for imagePath in imagePaths:
 
-            PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+            PIL_img = image.open(imagePath).convert('L') # convert it to grayscale
             img_numpy = np.array(PIL_img,'uint8')
 
             id = int(os.path.split(imagePath)[-1].split(".")[1])
-            faces = detector.detectMultiScale(img_numpy)
+            faces = self.detector.detectMultiScale(img_numpy)
 
             for (x,y,w,h) in faces:
                 faceSamples.append(img_numpy[y:y+h,x:x+w])
@@ -215,7 +236,21 @@ class App(Frame):
 
         return faceSamples,ids
 
+    def train(self):
+        path = 'dataset'
 
+        recognizer = cv2.face.LBPHFaceRecognizer_create()
+        self.detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+        print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+        faces,ids = self.getImagesAndLabels(path)
+        recognizer.train(faces, np.array(ids))
+
+        # Save the model into trainer/trainer.yml
+        recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
+
+        # Print the numer of faces trained and end program
+        print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
 
 app = App()
 app.master.title("Administrador del Sistema")
